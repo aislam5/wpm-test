e831cae96bdd10079211a3e09917a67a47857773
Abdullah Islam <abdullahislam900@gmail.com>
2025-06-14 15:37:15 -0400

    added functions for start screen and wmpTest. Tested out curses

--- wpm_test.py
+++ wpm_test.py
@@ -5,7 +5,7 @@ import curses
 from curses import wrapper
 
 """
-Print a sentence to type.
+Print a sentence to type. (Done)
 Wait for user to press Enter to start.
 Record the start time.
 Accept user input.
@@ -16,9 +16,33 @@ Display WPM, accuracy, and mistakes.
 """
 
 def main(stdscr):
-  stdscr.clear()
-  stdscr.addstr("Hello World!")
-  stdscr.refresh()
-  stdscr.getkey()
+  curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK) # foreground color green background color is white
+  curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLACK) #The integer is the ID if 1 again it will override the existing pair 
+  curses.init_pair(3, curses.COLOR_RED, curses.COLOR_BLACK)
+
+  key = start_screen(stdscr)
+  if(key.lower()!="y"):
+    return
+
+  """stdscr.clear() #Clears the entire Screen
+  stdscr.addstr(0,0,"This is the typing test and you shall not pass", curses.color_pair(1)) #Prints first number for row second foor column
+  stdscr.refresh() #Need to refresh before doing anything
+  key = stdscr.getkey() #This makes sure that the user types something aand then the screen goes away
+  print(key)"""
+  #if not in place then it will refresh super fast and we wont be able to see anything
+
+
+#First Thing we will ask the user if they want to play the game and if they choose Y then play the game 
+def start_screen(stdscr):
+  stdscr.clear() 
+  stdscr.addstr("Welcome! To the Speed Typing Test", curses.color_pair(2)) #Prints first number for row second foor column
+  stdscr.addstr("\nWould You Like to Out Your Fingers to the Test Y/N: ", curses.color_pair(2))
+  stdscr.refresh() 
+  key = stdscr.getkey()
+  return key
+  
+def wpmTest(stdscr):
+  pass
+
 
 wrapper(main)
