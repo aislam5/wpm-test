f1dc7bd1d80889eb3259ecdd433988518ad0f890
Abdullah Islam <abdullahislam900@gmail.com>
2025-06-15 09:38:17 -0400

    stuck on ord. MAking it so that the user can write

--- wpm_test.py
+++ wpm_test.py
@@ -23,6 +23,7 @@ def main(stdscr):
   key = start_screen(stdscr)
   if(key.lower()!="y"):
     return
+  wpmTest(stdscr)
 
   """stdscr.clear() #Clears the entire Screen
   stdscr.addstr(0,0,"This is the typing test and you shall not pass", curses.color_pair(1)) #Prints first number for row second foor column
@@ -42,7 +43,35 @@ def start_screen(stdscr):
   return key
   
 def wpmTest(stdscr):
-  pass
+  #print target text and then user to print it 
+  target_text = "Hey You Boy This is the Target Text for this text"
+  current_text = []
+  stdscr.clear() 
+
+  stdscr.addstr(target_text, curses.color_pair(2))
+  stdscr.refresh()
+
+  #Take a while loop and ask the user to type something in and everytime they type something it appears as either green or black
+  while True:
+
+    stdscr.clear() #clear so it doesnt keep the old text everytime
+    stdscr.addstr(target_text)
+
+    #loop through current_text and place the charcaters on the screen
+    for char in current_text:
+      stdscr.addstr(char, curses.color_pair(1))
+
+    stdscr.refresh() #clears the screen after adding each time so that it doesnt make a long list of things
+   
+    key = stdscr.getkey()
+    if ord(key) == 27: #ASCII REPRESENTAION OF ESCAPE KEY
+      break
+
+    if key in ("KEY_BACKSPACE"):
+      if len(current_text) > 0:
+        current_text.pop() #will remove the last chracter of the list
+    else:
+      current_text.append(key) #makes sure that if the above backspace is the case it is not being added to the text
 
 
 wrapper(main)
